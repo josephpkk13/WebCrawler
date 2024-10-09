53318f46594fefd77a60d9f67e8de8e0f1b2a924
Joseph Poon <josephpkk13@gmail.com>
2024-10-09 10:24:30 -0700

    - fix main file

--- crawler.py
+++ crawler.py
@@ -1,4 +1,9 @@
 import requests
 
 url = "https://www.turo.com"
-headers = {'User-Agent': 
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36'}
+
+response = requests.get(url, headers = headers)
+
+with open('output.html', 'w', encoding='utf-8') as f:
+    f.write(response.text)
